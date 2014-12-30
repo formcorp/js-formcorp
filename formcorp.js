fa1b62bcd8680f9aa82c129ceebd20a336f57053
Alex Berriman <alexb@developer.mixxi.com>
2014-12-30 00:49:19 -0800

    Render a basic textfield only form

--- formcorp.js
+++ formcorp.js
@@ -1,7 +1,13 @@
 var fc = new function ($) {
     var apiUrl = '//192.168.0.106:9001/';
 
-    // Send off an api call
+    /**
+     * Send off an API call.
+     * @param uri
+     * @param data
+     * @param type
+     * @param callback
+     */
     var api = function (uri, data, type, callback) {
         if (type === undefined || typeof(type) != 'string' || ['GET', 'POST'].indexOf(type.toUpperCase()) == -1) {
             type = 'GET';
@@ -31,7 +37,11 @@ var fc = new function ($) {
         });
     }
 
-    // Initialise the FC object
+    /**
+     * Initialise the formcorp object.
+     * @param publicKey
+     * @param container
+     */
     this.init = function (publicKey, container) {
         this.publicKey = publicKey;
         this.container = container;
@@ -66,15 +76,108 @@ var fc = new function ($) {
             });
         });
 
-        // Render the form
+        /**
+         * Render a text field.
+         * @param field
+         * @returns {string}
+         */
+        var renderTextfield = function (field) {
+            var html = '<input type="text" name="' + field._id.$id + '">';
+
+            return html;
+        }
+
+        /**
+         * Render a form stage.
+         */
         var render = function () {
             var currentStage = typeof(fc.currentStage) != 'undefined' ? fc.currentStage : 1,
                 currentStageIndex = currentStage - 1,
                 stage = fc.schema.stage[currentStageIndex];
 
+            console.log(stage);
             if (typeof(stage) != 'object') {
                 return;
             }
+
+            // @todo: page selection based on criteria
+            var html = '<h1>' + stage.label + '</h1>';
+            html += renderPage(stage.page[0]);
+
+            $(fc.jQueryContainer).html(html);
+        }
+
+        /**
+         * Render a page.
+         * @param page
+         * @returns {string}
+         */
+        var renderPage = function (page) {
+            // Page details
+            var pageDiv = '<div class="page">';
+            if (typeof(page.label) == 'string' && page.label.length > 0) {
+                pageDiv += '<h2>' + page.label + '</h2>';
+            }
+            if (typeof(page.description) == 'string' && page.description.length > 0) {
+                pageDiv += '<h3>' + page.description + '</h3>';
+            }
+
+            // Render page sections
+            if (page.section.length > 0) {
+                pageDiv += renderPageSections(page.section);
+            }
+
+            pageDiv += '</div>';
+
+            return pageDiv;
+        }
+
+        /**
+         * Render page sections.
+         * @param sections
+         * @returns {string}
+         */
+        var renderPageSections = function (sections) {
+            var html = '';
+
+            for (var x = 0; x < sections.length; x++) {
+                var section = sections[x],
+                    sectionHtml = '<div class="section">';
+
+                if (typeof(section.label) == 'string' && section.label.length > 0) {
+                    sectionHtml += '<h4>' + section.label + '</h4>';
+                }
+
+                if (typeof(section.description) == 'string' && section.description.length > 0) {
+                    sectionHtml += '<p>' + section.description + '</p>';
+                }
+
+                // Render the fields
+                if (typeof(section.field) != 'undefined' && section.field.length > 0) {
+                    for (var y = 0; y < section.field.length; y++) {
+                        var field = section.field[y],
+                            fieldHtml = '<div class="field">';
+
+                        if (typeof(field.config) == 'object' && typeof(field.config.label) == 'string' && field.config.label.length > 0) {
+                            fieldHtml += '<label>' + field.config.label + '</label>';
+                        }
+
+                        switch (field.type) {
+                            case 'text':
+                                fieldHtml += renderTextfield(field);
+                                break;
+                        }
+
+                        fieldHtml += '</div>';
+                        sectionHtml += fieldHtml;
+                    }
+                }
+
+                sectionHtml += '</div>';
+                html += sectionHtml;
+            }
+
+            return html;
         }
     }
 
